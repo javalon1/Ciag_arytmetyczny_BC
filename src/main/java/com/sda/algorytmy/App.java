10c36e28409b76f5b1db31c51c6d2df19ef19f89
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-18 12:15:16 +0100

    Final

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,22 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a1 = scanner.nextInt();
+        int d = scanner.nextInt();
+        int n = scanner.nextInt();
+
+
+        for (int i = 2 ; i <= n; i++){
+            System.out.println(a1 + (i-1) * d);
+        }
+
     }
 }
